2ca9cc2745d57e24461f310488806e7f75cc0f0c
Rohit LIVE <therohitkhanna@live.com>
2018-11-28 16:49:53 +0530

    UC4: Closure, with Map(ES6) data strcture

---
+++ JS-ES6/use case/UC4/index.js
@@ -0,0 +1,25 @@
+function getX() {
+  // Data Structure for Cache
+  // obj = {}; // ES 5
+  objMap = new Map(); // ES 6
+
+  return function(x) {
+    // if (obj[x]) {  //ES 5
+    if (objMap.get(x)) {
+      console.log("Available in Cache");
+    } else {
+      //  obj[x] = x; // ES 5
+      objMap.set(x, x);
+      console.log("Adding");
+    }
+  };
+}
+
+let o = new getX();
+o(3);
+o(3);
+o(1);
+o(2);
+o(4);
+o(3);
+o(4);
