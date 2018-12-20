297d7aedebaddeb4fb43cc855ebb00e864c04a37
Rohit LIVE <therohitkhanna@live.com>
2018-12-20 10:27:05 +0530

    Added Middleware

--- DevOps/hands-on/Express-demo/index.js
+++ DevOps/hands-on/Express-demo/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const ctrl = require("./OrderController");
 const app = express();
-
-
 app.use("/order", ctrl);
+app.use('/',middleware);
+
+
 
 
 
 app.listen(4001, () => {
   console.log("server started at port 4001");
 });
+
+
+function middleware(req,res,next){
+res.end('Welcome to Home page. Use "/order" path');
+
+}
